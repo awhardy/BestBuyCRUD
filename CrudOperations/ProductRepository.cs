4161e95d0642a76287bb847460fff80baf1db218
awhardy <hardy.andreww@gmail.com>
2018-10-22 10:31:40 -0500

    InitialCommit

---
+++ CrudOperations/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace CrudOperations
+{
+    class ProductRepository
+    {
+        public ProductRepository(string connectionString)
+        {
+            connStr = connectionString;
+        }
+
+        private string connStr;
+    }
+}
---
+++ CrudOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace CrudOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            string jsonText = File.ReadAllText("appsettings.development.json");
+#else
+            string josonText = File.ReadAllText("appsettings.release.json")
+#endif
+            string connStr = JObject.Parse(jsonText)["ConnectionStrings"]["DefaultConnection"].ToString();
+
+            ProductRepository prod = new ProductRepository(connStr);
+
+            ProductRepository repo = new ProductRepository(connStr);
+        }
+    }
+}
